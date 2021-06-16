5241d5d4ae76efafcde4a44db6ab353579155512
V_O <v9160628646@gmail.com>
2021-06-16 15:33:17 +0300

    add options tab and changeTheme toggle button

---
+++ ViewModels/OptionsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VOlkin.ViewModels
+{
+    public class OptionsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private bool _isDarkThemeOn = new PaletteHelper().GetTheme().GetBaseTheme() == BaseTheme.Dark;
+        public bool IsDarkThemeOn
+        {
+            get { return _isDarkThemeOn; }
+            set
+            {
+                _isDarkThemeOn = value;
+                ModifyTheme(value);
+            }
+        }
+
+        private void ModifyTheme(bool isDarkTheme)
+        {
+            var paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
